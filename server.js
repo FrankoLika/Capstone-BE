d582b19399fb356ef84dc03729fe053240596221
Lika Franko <likafranko01@gmail.com>
2023-06-22 17:09:38 +0200

    users route created and tested

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const usersRoute = require('./routes/users')
+
 const PORT = 5050;
 const app = express()
 
 dotenv.config()
 app.use(express.json())
 app.use(cors())
+app.use('/', usersRoute)
 
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,

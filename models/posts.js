5052cb129b382dcf967eb1625ad1844dcbe2e09e
Lika Franko <likafranko01@gmail.com>
2023-06-23 15:04:28 +0200

    post model created

---
+++ models/posts.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: false,
+        default: "https://tse2.mm.bing.net/th?id=OIP.tT6tBsjQq6RzVEjT-nHiXgHaHa&pid=Api&P=0&h=180"
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+}, { timestamps: true, strict: true })
+
+module.exports = mongoose.model('postModel', postSchema, 'Posts')
